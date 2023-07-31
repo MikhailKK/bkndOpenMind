97713891ff0daa07a0957e2c9307a7df9737063a
Mikka <telfax@yandex.ru>
2023-07-31 11:33:36 +0300

    first app func

--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"bkndOpenMind/database"
 	"bkndOpenMind/routes"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
 
+func welcome(c *fiber.Ctx) error {
+	return c.SendString("Welcome this API")
+}
+
 func main() {
 	app := fiber.New()
+
+	app.Get("/api", welcome)
 
 	// Initialize database connection
 	database.ConnectDB()
@@ -20,5 +27,6 @@ func main() {
 	routes.SetupRoutes(app)
 
 	// Start the server
-	app.Listen(":3000")
+	// app.Listen(":3000")
+	log.Fatal(app.Listen(":3000"))
 }
